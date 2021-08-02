40f1408544d27187cec07d6a7968608b1baab1b2
songyifan <songyifan@kuaishou.com>
2021-08-02 23:03:11 +0800

    Texture Test

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -22,6 +22,7 @@
 #include "imgui/imgui_impl_opengl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 //struct ShaderProgramSource
 //{
@@ -283,6 +284,7 @@ int main(void)
         currentTest = testMenu;
 
         testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+        testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
 
         //test::TestClearColor test;
 
